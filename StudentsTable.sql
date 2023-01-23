44e26d96dba6a6e2a2c8d6bb47e7c8dbb1a6e2ff
Sam Alexander <61659157+EslamFouad1@users.noreply.github.com>
2023-01-23 15:26:54 +0200

    Add files via upload

---
+++ StudentsTable.sql
@@ -0,0 +1,39 @@
+CREATE TABLE Students (
+    ID int,
+    NAME varchar(255),
+    CLASS varchar(255),
+  	GPA int
+);
+
+INSERT INTO Students(ID, NAME, CLASS, GPA)
+VALUES (10015, "Smith, John", "Sophomore", 3.01);
+
+INSERT INTO Students(ID, NAME, CLASS, GPA)
+VALUES (10167, "Jones, Wendy", "Junior", 2.85);
+
+INSERT INTO Students(ID, NAME, CLASS, GPA)
+VALUES (10175, "Smith, Jane", "Senior", 3.92);
+
+INSERT INTO Students(ID, NAME, CLASS, GPA)
+VALUES (10188, "Wales, Sam", "Senior", 3.25);
+
+INSERT INTO Students(ID, NAME, CLASS, GPA)
+VALUES (10200, "Roberts, Sally", "Freshman", 4.00);
+
+INSERT INTO Students(ID, NAME, CLASS, GPA)
+VALUES (10208, "Green, Patrick", "Freshman", 3.95);
+
+INSERT INTO Students(ID, NAME, CLASS, GPA)
+VALUES (10226, "Nelson, Amy", "Sophomore", 2.95);
+
+INSERT INTO Students(ID, NAME, CLASS, GPA)
+VALUES (10334, "Roberts, Jane", "Senior", 3.81);
+
+INSERT INTO Students(ID, NAME, CLASS, GPA)
+VALUES (10387, "Taylor, Susan", "Sophomore", 2.15);
+
+INSERT INTO Students(ID, NAME, CLASS, GPA)
+VALUES (10400, "Logan, Mark", "Junior", 3.33);
+
+INSERT INTO Students(ID, NAME, CLASS, GPA)
+VALUES (10485, "Brown, Jessica", "Sophomore", 2.91);
